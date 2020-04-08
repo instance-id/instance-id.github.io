3ac65a4d4dbf62a5a3c155e6fc93336fca78710b
Dan <dan@instance.id>
2020-04-07 20:35:08 -0500

    rebuilding site 04/07/2020 20:35:08

--- deploy.ps1
+++ deploy.ps1
@@ -1,26 +1,29 @@
 Write-Host "Deploying updates to GitHub..."
 
+$tmp = "E:/GitHub/tmp"
 $dir = "E:/GitHub/instanceid-hugo"
 $sourceDirectory = "E:/GitHub/instanceid-hugo/public/*"
 $destinationDirectory = "E:/GitHub/instance-id.github.io"
 
 # Build the project
-#hugo
 & hugo -v
 $msg = "rebuilding site $(Get-Date)"
 
 Write-Host "Committing local repo"
 & git add -A
 & git commit -m $msg
-& git push origin master
+& git push origin site_files
 
 Write-Host "Copying $sourceDirectory to $destinationDirectory"
-Copy-item -Force -Recurse -Verbose -Path $sourceDirectory -Destination $destinationDirectory
-Set-Location $destinationDirectory
-
+Copy-item -Force -Recurse -Verbose -Path $destinationDirectory -Destination $tmp
+git checkout master
+Copy-item -Force -Recurse -Verbose -Path $tmp -Destination $destinationDirectory
 
 & git add -A
 & git commit -m $msg
 & git push origin master
 
-Set-Location $dir
+git checkout -b "site_files"
+
+New-BurntToastNotification -Silent -Text "Status Update:",
+'Site deployment complete'
